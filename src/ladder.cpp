7b49289fa5c2a84b2a800bfe52bf4cdc76da9463
kevinvafadouste <150042218+kevinvafadouste@users.noreply.github.com>
2025-03-16 04:54:29 -0700

    Update ladder.cpp

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -71,7 +71,6 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
 
 void print_word_ladder(const vector<string>& ladder) {
     if (ladder.empty()) {
-        cout << "No word ladder found." << endl;
         return;
     }
     cout << "Word ladder found: ";
