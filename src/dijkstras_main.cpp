b72aec97950913736675aa1bf67ef732c6c3971b
kevinvafadouste <150042218+kevinvafadouste@users.noreply.github.com>
2025-03-16 04:49:13 -0700

    Update dijkstras_main.cpp

--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main() {
     Graph G;
     string filename;
-    cout << "Enter the graph input file: ";
     cin >> filename;
     
     try {
@@ -16,10 +15,8 @@ int main() {
         cerr << e.what() << endl;
         return 1;
     }
-    
     vector<int> previous;
     vector<int> distances = dijkstra_shortest_path(G, 0, previous);
-    
     for (int i = 0; i < G.numVertices; i++) {
         vector<int> path = extract_shortest_path(distances, previous, i);
         cout << "Shortest path to vertex " << i << ": ";
