a4627eb8b4080b47b03c6a33585918dffa6ce183
kevinvafadouste <150042218+kevinvafadouste@users.noreply.github.com>
2025-03-16 04:54:00 -0700

    Update dijkstras.cpp

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -46,7 +46,6 @@ vector<int> extract_shortest_path(const vector<int>& distances, const vector<int
 
 void print_path(const vector<int>& path, int total) {
     if (path.size() < 2) {
-        cout << "No path found" << endl;
         return;
     }
     for (size_t i = 0; i < path.size(); ++i) {
