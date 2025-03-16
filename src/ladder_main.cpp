7a1d63a442164bc37a349f40a79fb772a43d717d
kevinvafadouste <150042218+kevinvafadouste@users.noreply.github.com>
2025-03-16 04:48:46 -0700

    Update ladder_main.cpp

--- src/ladder_main.cpp
+++ src/ladder_main.cpp
@@ -22,19 +22,12 @@ int main() {
     load_words(word_list, "words.txt");
     
     string start_word, end_word;
-    cout << "Enter start word: ";
     cin >> start_word;
-    cout << "Enter end word: ";
     cin >> end_word;
-    
-    if (start_word == end_word) {
-        cout << "Error: Start and end words must be different." << endl;
+    if (start_word == end_word)
         return 1;
-    }
-    
     vector<string> ladder = generate_word_ladder(start_word, end_word, word_list);
     print_word_ladder(ladder);
-    
     return 0;
 }
 
